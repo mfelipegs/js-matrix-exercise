412a5ae4329920e6465d5d3292a5f4540e857bb2
mfelipegs <matheusg.dasilva7@gmail.com>
2022-10-05 22:26:15 -0300

    Colored words

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,49 +1,53 @@
 function showMatrix() {
-//creating the array
-matrix = new Array;
 
-/*const characters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-const randomCharacter = characters[Math.floor(Math.random() * characters.length)];
-console.log(randomCharacter);*/
+    //creating the 2D array
+    matrix = new Array;
 
-const r1 = ["A", "B", "a", "G", "J", "N", "I", "B", "P", "W", "D", "B", "A", "b", "A", "S", "G", "E", "K", "Y"];
+    matrix = [
+    ["A", "B", "a", "G", "J", "N", "I", "B", "P", "W", "D", "B", "A", "b", "A", "S", "G", "E", "K", "Y"],
+    
+    ["Z", "J", "u", "N", "E", "A", "V", "A", "I", "S", "Z", "S", "A", "M", "u", "D", "K", "F", "Q", "Z"],
 
-const r2 = ["Z", "J", "u", "N", "E", "A", "V", "A", "I", "S", "Z", "S", "A", "M", "u", "D", "K", "F", "Q", "Z"];
+    ["B", "E", "d", "G", "A", "X", "S", "N", "P", "l", "a", "m", "b", "o", "r", "g", "h", "i", "n", "i"],
 
-const r3 = ["B", "E", "d", "G", "A", "X", "S", "N", "P", "l", "a", "m", "b", "o", "r", "g", "h", "i", "n", "i"];
+    ["s", "A", "i", "U", "K", "L", "V", "T", "b", "m", "w", "B", "C", "Q", "A", "J", "a", "V", "O", "X"],
 
-const r4 = ["s", "A", "i", "U", "K", "L", "V", "T", "b", "m", "w", "B", "C", "Q", "A", "J", "a", "V", "O", "X"];
+    ["a", "B", "l", "o", "t", "u", "s", "J", "I", "Q", "E", "B", "A", "M", "Y", "L", "F", "t", "A", "R"],
 
-const r5 = ["a", "B", "l", "o", "t", "u", "s", "J", "I", "Q", "E", "B", "A", "M", "Y", "L", "F", "t", "A", "R"];
+    ["l", "T", "A", "F", "A", "C", "Q", "H", "C", "m", "c", "l", "a", "r", "e", "n", "T", "Z", "t", "Q"],
 
-const r6 = ["l", "T", "A", "F", "A", "C", "Q", "H", "C", "m", "c", "l", "a", "r", "e", "n", "T", "Z", "t", "Q"];
+    ["e", "I", "Q", "W", "Q", "F", "A", "C", "K", "W", "A", "K", "N", "S", "p", "a", "g", "a", "n", "i"],
 
-const r7 = ["e", "I", "Q", "W", "Q", "F", "A", "C", "K", "W", "A", "K", "N", "S", "p", "a", "g", "a", "n", "i"];
+    ["e", "B", "A", "E", "D", "Z", "I", "B", "P", "R", "O", "L", "P", "O", "A", "T", "G", "V", "A", "S"],
 
-const r8 = ["e", "B", "A", "E", "D", "Z", "I", "B", "P", "R", "O", "L", "P", "O", "A", "T", "G", "V", "A", "S"];
+    ["n", "B", "A", "p", "o", "r", "s", "c", "h", "e", "R", "B", "R", "M", "Y", "R", "S", "P", "E", "F"],
 
-const r9 = ["n", "B", "A", "p", "o", "r", "s", "c", "h", "e", "R", "B", "R", "M", "Y", "R", "S", "P", "E", "F"];
+    ["L", "Z", "G", "G", "V", "L", "k", "o", "e", "n", "i", "g", "s", "e", "g", "g", "X", "V", "H", "Q"]];
 
-const r10 = ["L", "Z", "G", "G", "V", "L", "k", "o", "e", "n", "i", "g", "s", "e", "g", "g", "X", "V", "H", "Q"];
 
-matrix = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10]; 
+    //i = row
+    //j = column
+    document.write("<table border=0 class='tb1'>")
+    for(i = 0; i < 10; i++) {
 
-//i = row
-//j = column
-document.write("<table border=0 class='tb1'>")
-for(i = 0; i < 10; i++) {
+        document.write("<tr>");
 
-    document.write("<tr>");
+        for(j = 0; j < 20; j++) {
 
-    for(j = 0; j < 20; j++) {
-        div1 = document.getElementById('div_matrix');
-        div1.textContent = document.write("<td>" + matrix[i][j] + "</td>");
-        //div1.style.color = "black";
+            div1 = document.getElementById('div_matrix');
+            if(matrix[i][j] === matrix[i][j].toLowerCase()) {
+                div1.textContent = document.write("<td style='color: red;'>" + matrix[i][j].toUpperCase() + "</td>");
+            }
+            else {
+                console.log(matrix[i][j]);
+                console.log("aa");
+                div1.textContent = document.write(`<td>${matrix[i][j]}</td>`);
+            }
+        }
+
+        document.write("</tr>");
     }
-
-    document.write("</tr>");
-}
-document.write("</table>");
+    document.write("</table>");
 
 
 }
